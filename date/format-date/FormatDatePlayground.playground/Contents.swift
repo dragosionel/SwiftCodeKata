8c3d34b0d2c3d4bd0e6a238f140fad4d852de32d
Dragos Ionel <dionel@touchbistro.com>
2020-12-29 17:23:23 -0500

    Fixed the am/AM issue

--- date/format-date/FormatDatePlayground.playground/Contents.swift
+++ date/format-date/FormatDatePlayground.playground/Contents.swift
@@ -1,22 +1,29 @@
 import UIKit
+/*:
+ __Task: Display the current date in the following format: 9:30am / July 20, 2020__
 
-// Display the current date in the following format: 9:30am / July 20, 2020
-
+ Note:
+ - *am* and not *AM*
+ - month contains the full name (*July* and not *Jul*)
+*/
 let date =  Date()
 let formatter = DateFormatter()
-formatter.dateFormat = "HH:mm a / MMM dd, yyyy"
+formatter.dateFormat = "HH:mma / MMMM dd, yyyy"
+formatter.amSymbol = "am"
+formatter.pmSymbol = "pm"
 print(formatter.string(from: date))
 
+/*:
+ Interesting stuff: trying to convert using YYYY instead of yyyy will result in 2021. [More here](https://jayeshkawli.ghost.io/challenges-of-dateformatters/)
 
-///Interesting stuff: trying to convert using YYYY instead of yyyy will result in 2021
-///more here: https://jayeshkawli.ghost.io/challenges-of-dateformatters/
-///uncomment the following to try it
+ Uncomment the following to try it
+*/
 
 //let dec2020Formatter = DateFormatter()
 //dec2020Formatter.dateFormat = "yyyyMMdd"
 //let dec2020Date = dec2020Formatter.date(from: "20201227")!
 //
 //let wrongFormatter = DateFormatter()
-//wrongFormatter.dateFormat = "HH:mm a / MMM dd, YYYY" //used YYYY instead yyyy
+//wrongFormatter.dateFormat = "HH:mma / MMMM dd, YYYY" //used YYYY instead yyyy
 //
 //print(wrongFormatter.string(from: dec2020Date))
